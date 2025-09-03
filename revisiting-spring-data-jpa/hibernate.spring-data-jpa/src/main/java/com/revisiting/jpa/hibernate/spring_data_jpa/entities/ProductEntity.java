dccebbde04f6047bee1ec4188261f6cdd8407d77
Abhishek Sinha <sinha.abhishek18nov@gmail.com>
2025-09-03 10:28:30 +0530

    Spring Data JPA, creating custom queries by naming methods as per documentation and also by using @Query Annotation.

---
+++ revisiting-spring-data-jpa/hibernate.spring-data-jpa/src/main/java/com/revisiting/jpa/hibernate/spring_data_jpa/entities/ProductEntity.java
@@ -0,0 +1,57 @@
+package com.revisiting.jpa.hibernate.spring_data_jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name="product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique", columnNames = {"sku"}),
+                @UniqueConstraint(name= "title_price_unique", columnNames = {"title", "price"})
+        },
+        indexes = {
+                @Index(
+                        name="sku_index", columnList= "sku"
+                )
+        }
+)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String sku;
+
+    @Column
+    private String title;
+
+    @Column
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column
+    BigDecimal price ;
+
+    @Override
+    public String toString() {
+        return "\n{ " + "id: " + id + ",\nsku: " + sku + ",\n title: " + title +",\nquantity: " + quantity + ",\nprice: " + price + "\n}" ;
+    }
+
+}
--- revisiting-spring-data-jpa/hibernate.spring-data-jpa/src/test/java/com/revisiting/jpa/hibernate/spring_data_jpa/ApplicationTests.java
+++ revisiting-spring-data-jpa/hibernate.spring-data-jpa/src/test/java/com/revisiting/jpa/hibernate/spring_data_jpa/ApplicationTests.java
@@ -1,13 +1,46 @@
 package com.revisiting.jpa.hibernate.spring_data_jpa;
 
+import com.revisiting.jpa.hibernate.spring_data_jpa.entities.ProductEntity;
+import com.revisiting.jpa.hibernate.spring_data_jpa.repositories.ProductRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class ApplicationTests {
+    @Autowired
+    private ProductRepository productRepository;
 
-	@Test
+    @Test
 	void contextLoads() {
 	}
 
+    @Test
+    void testRepository () {
+        ProductEntity productX = ProductEntity.builder()
+                .title("Kurkure Chataka Masala")
+                .sku("ChatakaKurkure")
+                .quantity(333)
+                .price(BigDecimal.valueOf(19.50))
+                .build();
+        productRepository.save(productX);
+    }
+
+    @Test
+    void getRepository () {
+        // List<ProductEntity> listOfProducts = productRepository.findByCreatedAtAfter(LocalDateTime.of(2025, 9, 3, 0, 0 ,0));
+        // List<ProductEntity> listOfProducts = productRepository.findByQuantityAndPrice(4, BigDecimal.valueOf(42.5));
+        // List<ProductEntity> listOfProducts = productRepository.findByQuantityGreaterThanAndPriceLessThan(1000, BigDecimal.valueOf(42.5));
+        // List<ProductEntity> listOfProducts =productRepository.findByTitleLike("%kure%");
+        //List<ProductEntity> listOfProducts =productRepository.findByTitleContaining('kure');
+        //List<ProductEntity> listOfProducts =productRepository.findByTitleContainingIgnoreCase("KURE");
+        List<String> listOfProducts = productRepository.findAllTitleByTitleAndQuantity("%Chataka%", 1);
+        System.out.println("LIST RESULT AFTER 2st Sept :::" + listOfProducts.toString());
+    }
 }
