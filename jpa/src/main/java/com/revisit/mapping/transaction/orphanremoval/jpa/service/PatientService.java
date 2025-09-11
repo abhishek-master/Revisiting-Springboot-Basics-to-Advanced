2ca093c5e4b6b3d8e40b12f40385b487779cb2d8
Abhishek Sinha <sinha.abhishek18nov@gmail.com>
2025-09-11 10:42:38 +0530

    Understanding orphan removal in the Hibernate JPA.

--- jpa/src/main/java/com/revisit/mapping/transaction/orphanremoval/jpa/service/PatientService.java
+++ jpa/src/main/java/com/revisit/mapping/transaction/orphanremoval/jpa/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.revisit.mapping.transaction.orphanremoval.jpa.service;
 
+import com.revisit.mapping.transaction.orphanremoval.jpa.entities.Appointment;
+import com.revisit.mapping.transaction.orphanremoval.jpa.entities.Insurance;
 import com.revisit.mapping.transaction.orphanremoval.jpa.entities.Patient;
 import com.revisit.mapping.transaction.orphanremoval.jpa.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -25,4 +27,29 @@ public class PatientService {
          patientRepository.deleteById(id);
 
     }
+
+    @Transactional
+    public void getPatient(BigInteger id) {
+        Patient patient = patientRepository.findById(id).orElseThrow();
+        System.out.println(patient);
+        System.out.println(patient.getAppointments());
+    }
+
+    @Transactional
+    public void updatePatientAppointment(BigInteger patientId, Appointment appointment){
+
+    }
+
+    @Transactional
+    public void updatePatientInsurance(BigInteger patientId, Insurance insurance){
+        Patient patient = patientRepository.findById(patientId).orElseThrow();
+        patient.setInsurance(insurance);
+        insurance.setPatient(patient);
+        System.out.println("Patient ::::: " + patient);
+        System.out.println("Insurance ::::::: " + insurance);
+    }
 }
+
+/*
+*
+* */
