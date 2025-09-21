a8af66ae0e8d16869ba605c8cd0f17a5f2702bc8
Abhishek Sinha <111677709+abhishek-master@users.noreply.github.com>
2025-09-21 14:28:23 +0530

    Dive into Spring Security. (#21)
    * Boiler plate for understanding spring security, flow chart for how spring security internally works.
    
    * How to add customize the WebSecurity, writing inMemoryUsers for testing purposes.

---
+++ securityApplication/src/main/java/com/abhishek/security/securityApplication/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.abhishek.security.securityApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //This annotation let us tell our Springboot that now we are going to configure the spring security
+//filter chain. It tells that now we will be configuring the security stuff. SecurityFilterChain is one of those things.
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{
+        /*
+        * Request Matchers here are used to set access to the routes based on roles or say any route which needs
+        * to be public.
+        * */
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/posts").permitAll()
+                        .requestMatchers("posts/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    /*
+    * The below method is being used to create users in Memory and are maily used for testing cases
+    * */
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService(){
+        UserDetails normalUser = User
+                .withUsername("Abhishek")
+                .password(passwordEncoder().encode("Abhishek123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("AbhishekSinha")
+                .password(passwordEncoder().encode("Abhishek1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ securityApplication/src/main/java/com/abhishek/security/securityApplication/services/UserService.java
@@ -0,0 +1,21 @@
+package com.abhishek.security.securityApplication.services;
+
+import com.abhishek.security.securityApplication.exceptions.ResourceNotFoundException;
+import com.abhishek.security.securityApplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//@Service
+@RequiredArgsConstructor
+public class UserService  implements UserDetailsService {
+
+    private final UserRepository userRepository ;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException("User with Email " + username + " not found.")) ;
+    }
+}
