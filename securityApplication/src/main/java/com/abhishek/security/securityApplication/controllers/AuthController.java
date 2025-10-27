c68aac5b4dbf833b953a5ecd62520fd2cbaa5267
Abhishek Sinha <sinha.abhishek18nov@gmail.com>
2025-10-27 16:04:26 +0530

    Implementing RefreshTOken and refresh endpoint to complete the flow for Stateless AUTH.

--- securityApplication/src/main/java/com/abhishek/security/securityApplication/controllers/AuthController.java
+++ securityApplication/src/main/java/com/abhishek/security/securityApplication/controllers/AuthController.java
@@ -2,21 +2,30 @@ package com.abhishek.security.securityApplication.controllers;
 
 
 import com.abhishek.security.securityApplication.dto.LoginDTO;
+import com.abhishek.security.securityApplication.dto.LoginResponseDto;
 import com.abhishek.security.securityApplication.dto.SignUpDTO;
 import com.abhishek.security.securityApplication.dto.UserDTO;
 import com.abhishek.security.securityApplication.services.AuthService;
 import com.abhishek.security.securityApplication.services.UserService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path ="/auth")
 public class AuthController {
+
+    @Value("${deploy.environment}")
+    private String deployedEnv ;
+
     private final UserService userService;
     private final AuthService authService;
 
@@ -28,11 +37,29 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO, HttpServletResponse res) {
-        String token = authService.login(loginDTO);
-        Cookie cookie = new Cookie("Token", token);
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDTO loginDTO, HttpServletResponse res) {
+        LoginResponseDto response = authService.login(loginDTO);
+        Cookie cookie = new Cookie("refreshToken", response.getRefreshToken());
         cookie.setHttpOnly(true);
+        cookie.setSecure("development".equals(deployedEnv)); //This cookie can only be pass with https, for local host we disable it.
         res.addCookie(cookie);
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refreshToken (HttpServletRequest request){
+        Cookie [] cookies = request.getCookies();
+        String refreshToken = Arrays.stream(cookies).
+                filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(()-> new SecurityException("No Refresh Token Found !"));
+        return ResponseEntity.ok(authService.getNewAccessToken(refreshToken));
     }
 }
+
+/*
+* How to avoid getting refresh Token Compromised ?
+* We can minimize the compromise by securely storing the Refresh tokens in Same-Site, HTTP only and Secure Cookies
+* Using HTTPS for transferring refresh token.
+* */
